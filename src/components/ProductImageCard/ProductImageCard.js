c54bbdd4d0fd63c0cce4b7bea82596e6d59fb9ff
Artem <artem.mikheev.git@gmail.com>
2020-11-26 00:48:14 +0300

    Update component 'ProductImageCard' 1. added slider, 2. added the abiliti to resize image, 3. added buttons to resize image.

--- src/components/ProductImageCard/ProductImageCard.js
+++ src/components/ProductImageCard/ProductImageCard.js
@@ -1,36 +1,90 @@
-import React, { useState } from 'react'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import Slider from '@material-ui/core/Slider';
+import Typography from '@material-ui/core/Typography';
+import React from 'react'
 import './ProductImageCard.scss'
 
-export default function ProductImageCard(props){
-    const [imageUrl, setImageUrl] = useState(null)
-    console.log(props.arr)
-    const addFile = (event)=>{
-        let file =  event.target.files[0]
-        const reader =  new FileReader()
-        let fileData ={}
-        reader.onload = () =>{
-          fileData.src =  reader.result
-        }
-        reader.readAsDataURL(file)
-        let newfileUrl = URL.createObjectURL(file)
+export default function ProductImageCard({image, onChange, resizeHandler}){
+    function valueLabelFormat(image) {
+      return `${image}%`
+    }
 
-        setImageUrl(newfileUrl)
-      }
+    const handleChange = (event, newValue) => {
+      resizeHandler(newValue, image.id)
+    };
+
+    const theme = createMuiTheme({
+      overrides: {
+        MuiTypography: {
+          root: { color: '#BC9CFF' },
+          body1: { lineHeight: 'none' },
+          gutterBottom: { marginBottom: '0px' }
+        },
+        MuiSlider: {
+          root: { width: '170px' },
+          rail: { color: '#BC9CFF', height: '3px' },
+          track: { background: 'linear-gradient(90deg, #BC9CFF 0%, #8BA4F9 100%)', height: '3px' },
+          valueLabel: { color: '#8BA4F9'},
+          thumb: { color: '#8BA4F9'}
+        },
+      },
+    });
 
     return(
-            <div className ='ProductImageCard'>
-            <img 
-                src={imageUrl}
-                name = 'imagesProduct'
-                alt = 'no file' 
-                style = {{width: '250px', position: 'absolute', left: 0, top: 0}}
-            />
-            <input 
-                type = 'file'
-                accept = 'image/*'
-                onChange = {(event)=>addFile(event)}  
-               />
+      <div className ='ProductImageCard'>
+        <div className = 'sliderWrapper'>
+          {image.url
+          ?<ThemeProvider theme = {theme}>
+            <Typography id="non-linear-slider" gutterBottom>
+              Размер изображения
+            </Typography>
+            <div className = 'sliderConteiner'>
+              <i 
+                className='fa fa-minus-circle fa-2x'
+                onClick = {image.width > 0 ?()=>resizeHandler(image.width -1, image.id) :null} 
+              />
+              <Slider
+                value = {image.width}
+                min={0}
+                step={1}
+                max={100}
+                getAriaValueText={()=>valueLabelFormat(image.width)}
+                valueLabelFormat={()=>valueLabelFormat(image.width)}
+                onChange={handleChange}
+                valueLabelDisplay="auto"
+                aria-labelledby="non-linear-slider"
+              />
+              <i 
+                className='fa fa-plus-circle fa-2x' 
+                onClick = {image.width < 100 ?()=>resizeHandler(image.width +1, image.id) :null} 
+              />
+            </div>
+          </ThemeProvider>
+          : null }
         </div>
+        <div className = 'conteiner'>
+          {image.url
+          ?<div className = 'imageConteiner'>
+            <img 
+              src={image.url}
+              name = 'imagesProduct'
+              alt = 'no file' 
+              style = {{width: `${250+(250/100*image.width)}px`, position: 'absolute', left: 0, top: 0}}
+            />
+          </div>
+          :<div className = 'inputConteiner'>
+            <i className = 'fa fa-ban fa-5x' />
+            <span>Изображение отсутствует!<br/>Нажмите чтобы выбрать</span>
+            <input 
+              type = 'file'
+              accept = 'image/*'
+              onChange = {(event)=>onChange(event, image.id)}  
+            />
+          </div>
+          }
+        </div>
+        <button>Click</button>
+      </div>
         
     )
 }
--- src/components/ProductImagesSetings/ProductImagesSetings.js
+++ src/components/ProductImagesSetings/ProductImagesSetings.js
@@ -6,30 +6,67 @@ import './ProductImagesSetings.scss'
 export default function ProductImagesSetings(){
     const [imagesCards, setImagesCards] = useState([])
 
-    function addImage(){
-        
-        imagesCards.push('click')
+    const  resizeHandler = (value, id) =>{
+        imagesCards.map(imageData => {
+            if(imageData.id === id){
+                imageData.width = value   
+            }
+            return null
+        })
         setImagesCards([...imagesCards])
-        console.log(imagesCards)
     }
 
+    function addImage(){
+        const imageData = {
+            id: `images${Math.random()}`,
+            url: null,
+            left: 0,
+            top: 0,
+            width: 0
+        }
+        imagesCards.push(imageData)
+        setImagesCards([...imagesCards]) 
+    }
+
+    const addFile = (event, id)=>{
+        let file =  event.target.files[0]
+        const reader =  new FileReader()
+        let fileData ={}
+        reader.onload = () =>{
+          fileData.src =  reader.result
+        }
+        reader.readAsDataURL(file)
+        
+        imagesCards.map(imageData => {
+            if(imageData.id === id){
+                imageData.url = URL.createObjectURL(file)   
+            }
+            return null
+        })
+        setImagesCards([...imagesCards])
+      }
+    
     function getFormData (){
-        console.log('klisc')
         const imagesList = document.forms.imagesSetings.imagesProduct 
         for (var item of imagesList) {
-            console.log(item.getBoundingClientRect())
+            item.getBoundingClientRect()
           }
-        
-        
     }
-    console.log('return')
+    
     return(
         <div className = 'ProductImagesSetings'>
             <div className = 'isideBlock'>
-                <form name = 'imagesSetings'>
-                    {imagesCards.map(elem => (<div>{elem}</div>))}
-                    
-                </form>
+                <div className = 'imagesConteiner'>
+                    {imagesCards.map(image => (
+                        <ProductImageCard 
+                            key = {image.id}
+                            image = {image}
+                            onChange = {addFile}
+                            resizeHandler = {resizeHandler}
+                        />
+                    ))}
+                </div>
+               
                 <AddImageButton 
                     onClick = {addImage}
                 />
